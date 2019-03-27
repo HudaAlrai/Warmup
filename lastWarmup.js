07b11bbd8fc082c666d9437269a4b41dd2a411d7
HudaAlrai <h.alraee94@gmail.com>
2019-03-27 10:28:16 +0200

    solved

--- lastWarmup.js
+++ lastWarmup.js
@@ -6,5 +6,16 @@ Example:
 var nums = [2, 7, 11, 15], target = 9,
 twoSum(nums,9); //should return [0,1] because nums[0] + nums[1] = 2 + 7 = 9;
 */
-
+function twoSum (nums, target){
+	var  result = [];
+	for(var i = 0; i<nums.length; i++){
+		for (var j = i; j<nums.length; j++){
+			if(nums[i] + nums[j] === target){
+				result.push(i)
+				result.push( j)
+				}
+		}
+	}
+	return result;
+}
 
